b6b3971b37c5d4de0ed25f91353a3f6de6cb5c59
Kenji Ishii <ken-ishii@sx.jp.nec.com>
2016-09-21 16:45:19 +0900

    Add javascript tests for actions.modules.js
    This patch adds javascript tests for the following modules.
      horizon.dashboard.container-infra.cluster-templates.actions
      horizon.dashboard.container-infra.clusters.actions
    
    Change-Id: Ibac08dd132b18dc4fae224acfe407b675e841d5d
    (cherry picked from commit ebe35f5ac7e0265767ed2c0bf95e3679f8ad6617)

---
+++ magnum_ui/static/dashboard/container-infra/cluster-templates/actions.module.spec.js
@@ -0,0 +1,59 @@
+/**
+ * (c) Copyright 2016 NEC Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function() {
+  'use strict';
+
+  describe('register cluster template actions module', function() {
+    var registry;
+    beforeEach(module('horizon.dashboard.container-infra.cluster-templates.actions'));
+
+    beforeEach(inject(function($injector) {
+      registry = $injector.get('horizon.framework.conf.resource-type-registry.service');
+    }));
+
+    it('registers Create Cluster as an item action', function() {
+      var actions = registry.getResourceType('OS::Magnum::ClusterTemplate').itemActions;
+      expect(actionHasId(actions, 'createClusterAction')).toBe(true);
+    });
+
+    it('registers Delete ClusterTemplate as an item action', function() {
+      var actions = registry.getResourceType('OS::Magnum::ClusterTemplate').itemActions;
+      expect(actionHasId(actions, 'deleteClusterTemplateAction')).toBe(true);
+    });
+
+    it('registers Create ClusterTemplate as a batch action', function() {
+      var actions = registry.getResourceType('OS::Magnum::ClusterTemplate').batchActions;
+      expect(actionHasId(actions, 'createClusterTemplateAction')).toBe(true);
+    });
+
+    it('registers Delete ClusterTemplate as a batch action', function() {
+      var actions = registry.getResourceType('OS::Magnum::ClusterTemplate').batchActions;
+      expect(actionHasId(actions, 'batchDeleteClusterTemplateAction')).toBe(true);
+    });
+
+    function actionHasId(list, value) {
+      return list.filter(matchesId).length === 1;
+
+      function matchesId(action) {
+        if (action.id === value) {
+          return true;
+        }
+      }
+    }
+
+  });
+})();
---
+++ magnum_ui/static/dashboard/container-infra/clusters/actions.module.spec.js
@@ -0,0 +1,64 @@
+/**
+ * (c) Copyright 2016 NEC Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function() {
+  'use strict';
+
+  describe('register cluster actions module', function() {
+    var registry;
+    beforeEach(module('horizon.dashboard.container-infra.clusters.actions'));
+
+    beforeEach(inject(function($injector) {
+      registry = $injector.get('horizon.framework.conf.resource-type-registry.service');
+    }));
+
+    it('registers Show Certificate as an item action', function() {
+      var actions = registry.getResourceType('OS::Magnum::Cluster').itemActions;
+      expect(actionHasId(actions, 'showCertificateAction')).toBe(true);
+    });
+
+    it('registers Sign Certificate as an item action', function() {
+      var actions = registry.getResourceType('OS::Magnum::Cluster').itemActions;
+      expect(actionHasId(actions, 'signCertificateAction')).toBe(true);
+    });
+
+    it('registers Delete Cluster as an item action', function() {
+      var actions = registry.getResourceType('OS::Magnum::Cluster').itemActions;
+      expect(actionHasId(actions, 'deleteClusterAction')).toBe(true);
+    });
+
+    it('registers Create Cluster as a batch action', function() {
+      var actions = registry.getResourceType('OS::Magnum::Cluster').batchActions;
+      expect(actionHasId(actions, 'createClusterAction')).toBe(true);
+    });
+
+    it('registers Delete Cluster as a batch action', function() {
+      var actions = registry.getResourceType('OS::Magnum::Cluster').batchActions;
+      expect(actionHasId(actions, 'batchDeleteClusterAction')).toBe(true);
+    });
+
+    function actionHasId(list, value) {
+      return list.filter(matchesId).length === 1;
+
+      function matchesId(action) {
+        if (action.id === value) {
+          return true;
+        }
+      }
+    }
+
+  });
+})();
