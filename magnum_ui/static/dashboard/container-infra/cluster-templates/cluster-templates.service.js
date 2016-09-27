00014555e41648b4083a5d6be83bbef35a2f81dc
Kenji Ishii <ken-ishii@sx.jp.nec.com>
2016-09-27 10:45:52 +0900

    Move listFunction and add javascript tests for cluster services
    This patch move listFunction and urlFunction  in module.js into service.js
    It is same as horizon's implementation and it will be able to add tests.
    
    This patch adds javascript tests for the following modules.
     - horizon.dashboard.container-infra.clusters.service
     - horizon.dashboard.container-infra.clusterTemplates.service
    
    Change-Id: I4388e2e45dfa39e177ccb92df714118f0008f982

---
+++ magnum_ui/static/dashboard/container-infra/cluster-templates/cluster-templates.service.js
@@ -0,0 +1,60 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function() {
+  "use strict";
+
+  angular.module('horizon.dashboard.container-infra.cluster-templates')
+    .factory('horizon.dashboard.container-infra.cluster-templates.service',
+      clusterTemplatesService);
+
+  clusterTemplatesService.$inject = [
+    '$filter',
+    'horizon.app.core.openstack-service-api.magnum'
+  ];
+
+  /*
+   * @ngdoc factory
+   * @name horizon.dashboard.container-infra.cluster-templates.service
+   *
+   * @description
+   * This service provides functions that are used through the Cluster Templates
+   * features.  These are primarily used in the module registrations
+   * but do not need to be restricted to such use.  Each exposed function
+   * is documented below.
+   */
+  function clusterTemplatesService($filter, magnum) {
+    return {
+      getClusterTemplatesPromise: getClusterTemplatesPromise,
+      urlFunction: urlFunction
+    };
+
+    function getClusterTemplatesPromise(params) {
+      return magnum.getClusterTemplates(params).then(modifyResponse);
+
+      function modifyResponse(response) {
+        return {data: {items: response.data.items.map(addTrackBy)}};
+
+        function addTrackBy(clusterTemplate) {
+          clusterTemplate.trackBy = clusterTemplate.id;
+          return clusterTemplate;
+        }
+      }
+    }
+
+    function urlFunction(item) {
+      return 'project/ngdetails/OS::Magnum::ClusterTemplate/' + item.id;
+    }
+  }
+})();
---
+++ magnum_ui/static/dashboard/container-infra/clusters/clusters.service.js
@@ -0,0 +1,60 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function() {
+  "use strict";
+
+  angular.module('horizon.dashboard.container-infra.clusters')
+    .factory('horizon.dashboard.container-infra.clusters.service',
+      clustersService);
+
+  clustersService.$inject = [
+    '$filter',
+    'horizon.app.core.openstack-service-api.magnum'
+  ];
+
+  /*
+   * @ngdoc factory
+   * @name horizon.dashboard.container-infra.clusters.service
+   *
+   * @description
+   * This service provides functions that are used through the Clusters
+   * features.  These are primarily used in the module registrations
+   * but do not need to be restricted to such use.  Each exposed function
+   * is documented below.
+   */
+  function clustersService($filter, magnum) {
+    return {
+      getClustersPromise: getClustersPromise,
+      urlFunction: urlFunction
+    };
+
+    function getClustersPromise(params) {
+      return magnum.getClusters(params).then(modifyResponse);
+
+      function modifyResponse(response) {
+        return {data: {items: response.data.items.map(addTrackBy)}};
+
+        function addTrackBy(cluster) {
+          cluster.trackBy = cluster.id;
+          return cluster;
+        }
+      }
+    }
+
+    function urlFunction(item) {
+      return 'project/ngdetails/OS::Magnum::Cluster/' + item.id;
+    }
+  }
+})();
