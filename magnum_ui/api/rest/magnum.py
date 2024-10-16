ee4abd492a655ad64050a8bb85dab27ad13fa99f
Jake Yip <jake.yip@unimelb.edu.au>
2024-10-16 12:28:09 +1100

    Fix resize for CAPI clusters
    Clusters created by the magnum-capi-helm driver do not have a heat stack, so
    resize currently fails as it depends on the heat stack for a list of worker
    nodes. To fix this, we change to querying the nodegroup information and
    use that instead.
    
    We keep the old heat stack way for now, as that is the way to get a list
    of worker nodes to select candidate(s) for scaling down. This is not
    available in capi driver yet, so capi will return an empty list of
    worker nodes.
    
    Change-Id: I0d66fc02a7f2c608a4a0b09b98c343017e04ed41
    (cherry picked from commit a5ff9ecb4fb84e95c266657b5aabdefe33010983)

--- magnum_ui/api/rest/magnum.py
+++ magnum_ui/api/rest/magnum.py
@@ -25,6 +25,7 @@ from django.views import generic
 from magnum_ui.api import heat
 from magnum_ui.api import magnum
 
+from heatclient import exc as heatexc
 from openstack_dashboard import api
 from openstack_dashboard.api import neutron
 from openstack_dashboard.api.rest import urls
@@ -237,18 +238,29 @@ class ClusterResize(generic.View):
             print(e)
             return HttpResponseNotFound()
 
-        stack = heat.stack_get(request, cluster["stack_id"])
-        search_opts = {"name": "%s-" % stack.stack_name}
-        servers = api.nova.server_list(request, search_opts=search_opts)[0]
+        try:
+            ngs = magnum.nodegroup_list(request, cluster_id)
+            nodegroups = [n.to_dict() for n in ngs]
+        except AttributeError:
+            return HttpResponseNotFound()
 
+        try:
+            stack = heat.stack_get(request, cluster["stack_id"])
+        except heatexc.HTTPNotFound:
+            stack = None
         worker_nodes = []
-        for server in servers:
-            if (server.name.startswith("%s-minion" % stack.stack_name) or
-                    server.name.startswith("%s-node" % stack.stack_name)):
-                worker_nodes.append({"name": server.name, "id": server.id})
+        if stack:
+            search_opts = {"name": "%s-" % stack.stack_name}
+            servers = api.nova.server_list(request, search_opts=search_opts)[0]
+
+            for server in servers:
+                if (server.name.startswith("%s-minion" % stack.stack_name) or
+                        server.name.startswith("%s-node" % stack.stack_name)):
+                    worker_nodes.append({"name": server.name, "id": server.id})
 
         return {"cluster": change_to_id(cluster),
-                "worker_nodes": worker_nodes}
+                "worker_nodes": worker_nodes,
+                "nodegroups": nodegroups}
 
     @rest_utils.ajax(data_required=True)
     def post(self, request, cluster_id):
