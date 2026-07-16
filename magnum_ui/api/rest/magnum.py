0284de69fb97335885709d0d4c897d4ace29e508
Michal Nasiadka <mnasiadka@gmail.com>
2026-07-16 10:52:40 +0200

    Remove Heat driver integration and rename stack_id to cluster_id
    The Heat driver has been removed from Magnum core. Remove all Heat-specific
    code from magnum-ui:
    
    - Delete magnum_ui/api/heat.py (Heat client wrapper, now dead)
    - Remove python-heatclient from requirements.txt
    - Remove Heat stack lookup from ClusterResize.get(); worker_nodes is now
      always empty since there is no Heat stack to query Nova against
    - Drop the heat and heatclient imports from magnum_ui/api/rest/magnum.py
    
    Rename the cluster detail panel to reflect the API field rename introduced
    in Magnum microversion 1.13 (stack_id -> cluster_id):
    
    - overview.html: "Stack" section -> "Cluster", "Stack ID" -> "Cluster ID",
      ctrl.cluster.stack_id -> ctrl.cluster.cluster_id, "Stack Faults" -> "Faults"
    
    Change-Id: Iea062ab499a5de27e30c6c03b42614779360545e
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>

--- magnum_ui/api/rest/magnum.py
+++ magnum_ui/api/rest/magnum.py
@@ -22,11 +22,8 @@ from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.views import generic
 
-from magnum_ui.api import heat
 from magnum_ui.api import magnum
 
-from heatclient import exc as heatexc
-from openstack_dashboard import api
 from openstack_dashboard.api import neutron
 from openstack_dashboard.api.rest import urls
 from openstack_dashboard.api.rest import utils as rest_utils
@@ -248,22 +245,8 @@ class ClusterResize(generic.View):
         except AttributeError:
             return HttpResponseNotFound()
 
-        try:
-            stack = heat.stack_get(request, cluster["stack_id"])
-        except heatexc.HTTPNotFound:
-            stack = None
-        worker_nodes = []
-        if stack:
-            search_opts = {"name": "%s-" % stack.stack_name}
-            servers = api.nova.server_list(request, search_opts=search_opts)[0]
-
-            for server in servers:
-                if (server.name.startswith("%s-minion" % stack.stack_name) or
-                        server.name.startswith("%s-node" % stack.stack_name)):
-                    worker_nodes.append({"name": server.name, "id": server.id})
-
         return {"cluster": change_to_id(cluster),
-                "worker_nodes": worker_nodes,
+                "worker_nodes": [],
                 "nodegroups": nodegroups}
 
     @rest_utils.ajax(data_required=True)
